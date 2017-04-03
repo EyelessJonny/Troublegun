cc26fca74bd25bdc04d51e08f748df6d48af1d2b
Jonny Butler <EyelessJonny@users.noreply.github.com>
2017-04-03 12:20:54 +0100

    Update troublegun.py

--- troublegun.py
+++ troublegun.py
@@ -9,15 +9,13 @@ class Troublegun:
     def mainF(self):
         """Main function"""
         userinput = input("Please select a task number: '1', '2' or '3'\n> ")
-        """Load Task 1"""
+        """Load Tasks"""
         if (userinput == "1"):
             from Task1 import main
             main.main.mainF()
-        """Load Task 2"""
         elif (userinput == "2"):
             from Task2 import main
             main.main.mainF()
-        """Load Task 3"""
         elif (userinput == "3"):
             from Task3 import main
             main.main.mainF()
